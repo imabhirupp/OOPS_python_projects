366bdf505694e089c00b093d605fe535092ffd3f
Abhirup Sinha <91724776+imabhirupp@users.noreply.github.com>
2024-08-20 16:24:20 +0530

    Add files via upload

--- NATO Alphabet Project/main.py
+++ NATO Alphabet Project/main.py
@@ -7,6 +7,15 @@ phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
 #print(phonetic_dict)
 
 # Creating a list of words using the phonetic_dict for each letter of the user_input
-user_input = input("Enter a word\n").upper()
-list_words = [phonetic_dict[letter] for letter in user_input]
-print(list_words)
+
+def phonetic():
+    user_input = input("Enter a word\n").upper()
+    try:
+        list_words = [phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Illegal Record was entered")
+        phonetic()
+    else:
+        print(list_words)
+
+phonetic()
